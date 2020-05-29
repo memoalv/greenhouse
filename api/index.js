c7180759b43f86618b4c30bce1a603587aec17ef
Guillermo Alvarez <gjalvarez09@gmail.com>
2020-05-28 19:05:51 -0600

    listen for unhandled exceptions

--- api/index.js
+++ api/index.js
@@ -5,6 +5,15 @@ const database = "greenhouse"
 
 var app = require("./app")
 
+process.on('uncaughtException', (error)  => {
+  console.log(error);
+})
+
+process.on('unhandledRejection', (error, promise) => {
+  console.log('Unhandled promise rejection: ', promise);
+  console.log('Error: ', error );
+});
+
 mongoose
   .connect(`mongodb://localhost:${dbPort}/${database}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
