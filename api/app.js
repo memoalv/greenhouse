158327963f28b3b044430daeef3056263591c4d8
Guillermo Alvarez <gjalvarez09@gmail.com>
2020-07-01 21:34:54 -0600

    added cors package

--- api/app.js
+++ api/app.js
@@ -4,9 +4,11 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const routes = require("./routes/index")
 const authMiddleware = require("./middlewares/auth")
+const cors = require("cors")
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(authMiddleware)
