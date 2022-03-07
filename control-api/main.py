51507a41882787033f1bd9689ca5837fc3a2d7a9
Guillermo Alvarez <gjalvarez09@gmail.com>
2022-03-06 17:52:22 -0700

    Adds logger endpoint

--- control-api/main.py
+++ control-api/main.py
@@ -1,19 +1,37 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
+import requests as r
+import json
 
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
+
+class DataPoint(BaseModel):
+  node: str
+  temperature: float
+  air_humidity: float
+  lux: float
+  soil_humidity: Optional[float] = None
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    print(os.getenv("AUTH_KEY"))
-    return {"Hello": "World"}
+def index():
+  return "OK"
 
+@app.post("/log")
+def log_data(data_point: DataPoint):
+  response = r.post(
+    f"{os.getenv('MONITORING_API_URL')}/climate",
+    headers={'Authorization': os.getenv('AUTH_KEY') },
+    json=json.loads(data_point.json())
+  )
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+  if response:
+    print('post success')
+  else: 
+    print('An error has occurred.')
+
+  return "Success"
