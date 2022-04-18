87fa544310fa69f9ada6f67b050232574ecd24f0
Guillermo Alvarez <gjalvarez09@gmail.com>
2022-04-17 19:48:52 -0600

    Updates micrpython scripts

--- sensors/half_node.py
+++ sensors/half_node.py
@@ -1,5 +1,5 @@
 import network
-from machine import Pin, I2C, ADC
+from machine import Pin, I2C
 from time import sleep
 import dht
 from veml7700 import VEML7700
@@ -9,8 +9,8 @@ import ujson
 wifi_network = "greenhouse-main"
 wifi_password = "greenhouse_wsn"
 control_api = "http://10.10.10.1:8000"
-station = network.WLAN(network.STA_IF)
 
+station = network.WLAN(network.STA_IF)
 if not station.isconnected():
     print("connecting to network...")
     station.active(True)
@@ -23,9 +23,6 @@ print("network config:", station.ifconfig())
 # initialize temperature and humidity sensor
 dht_sensor = dht.DHT22(Pin(4))
 
-# initialize soil moisture sensor
-adc = ADC(Pin(34))
-
 # initialize light sensor
 i2c = I2C(1, scl=Pin(22), sda=Pin(21), freq=10000)
 lux_sensor = VEML7700(adress=0x10, i2c=i2c, it=100, gain=1 / 8)
@@ -33,7 +30,6 @@ lux_sensor = VEML7700(adress=0x10, i2c=i2c, it=100, gain=1 / 8)
 while True:
     temp = None
     hum = None
-    soil_moisture = None
     lux = None
 
     try:
@@ -43,23 +39,18 @@ while True:
     except OSError as _:
         print("Failed to read DHT22.")
     try:
-        soil_moisture = adc.read_u16() / 1023 * 3.3
-    except OSError as _:
-        print("Failed to read soil moisture.")
-    try:
         lux = lux_sensor.read_lux()
     except OSError as _:
         print("Failed to read VEML.")
-    print(temp, hum, soil_moisture, lux)
+    print(temp, hum, lux)
 
     request_data = ujson.dumps({
-       "node": "node 2",
-       "temperature": temp,
-       "air_humidity": hum,
-       "lux": lux,
-       "soil_humidity": soil_moisture
-    })
-
+        "node": "node 1",
+        "temperature": temp,
+        "air_humidity": hum,
+        "lux": lux
+        })
     res = req.post(url=f"{control_api}/log", data=request_data)
     print(res.text)
+    
     sleep(300) # 5 min
