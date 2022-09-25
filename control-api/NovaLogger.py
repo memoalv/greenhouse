9d5ed6a50dbf991c9e0fcd4023232f29165edee9
Guillermo Alvarez <43304616+memoalv@users.noreply.github.com>
2022-09-25 15:18:45 -0600

    Update NovaLogger.py

--- control-api/NovaLogger.py
+++ control-api/NovaLogger.py
@@ -5,7 +5,6 @@ class NovaLogger:
   __device_key = os.getenv('NOVA_DEVICE_KEY')
 
   def __init__(self):
-    print(self.__device_key)
     self.hologram = HologramCloud({'devicekey': self.__device_key}, network='cellular')
 
   def send_to_influx(self, given_data_point):
